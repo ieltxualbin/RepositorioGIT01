149fea44896024ff5144ee40f8a2d055e0250546
Ieltxu Albin <ieltxu.albin02@somo.eus>
2024-12-16 08:25:14 +0100

    Commit Inicial Clase Empleado

---
+++ src/paqueteGIT/Empleado.java
@@ -0,0 +1,10 @@
+package paqueteGIT;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private byte edad;
+	private float sueldo;
+	private boolean hijos; // True si tiene hij@s, false en caso contrario
+}
